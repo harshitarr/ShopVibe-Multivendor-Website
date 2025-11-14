8e780d96d459de46852a7d51d9ce99396fef6cd2
Harshita <harshitarera@gmail.com>
2025-11-14 12:41:53 +0530

    inngest-update

---
+++ inngest/functions.js
@@ -0,0 +1,54 @@
+import { inngest } from './client'
+import dbConnect from '@/lib/mongodb'
+import User from '@/lib/models/User'
+
+// Inngest Function to save user data to database
+export const syncUserCreation = inngest.createFunction(
+  { id: 'sync-user-create' },
+  { event: 'clerk/user.created' },
+  async ({ event }) => {
+    await dbConnect(); // Connect to MongoDB
+    
+    const { data } = event;
+    await User.create({
+      _id: data.id, // Mongoose uses _id for custom IDs
+      id: data.id,
+      email: data.email_addresses[0].email_address,
+      name: `${data.first_name} ${data.last_name}`,
+      image: data.image_url,
+      cart: {} // Default empty cart
+    });
+  }
+)
+
+// Inngest Function to update user data in database
+export const syncUserUpdation = inngest.createFunction(
+  { id: 'sync-user-update' },
+  { event: 'clerk/user.updated' },
+  async ({ event }) => {
+    await dbConnect(); // Connect to MongoDB
+    
+    const { data } = event;
+    await User.findByIdAndUpdate(
+      data.id,
+      {
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+      { new: true } // Returns updated document
+    );
+  }
+)
+
+// Inngest Function to delete user from database
+export const syncUserDeletion = inngest.createFunction(
+  { id: 'sync-user-delete' },
+  { event: 'clerk/user.deleted' },
+  async ({ event }) => {
+    await dbConnect(); // Connect to MongoDB
+    
+    const { data } = event;
+    await User.findByIdAndDelete(data.id);
+  }
+)

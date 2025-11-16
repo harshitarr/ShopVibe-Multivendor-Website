93a965f95e701c89212b468302b516b2c40a5f90
Harshita <harshitarera@gmail.com>
2025-11-16 21:45:04 +0530

    update-imagekit

--- app/api/store/product/route.js
+++ app/api/store/product/route.js
@@ -7,9 +7,9 @@ import ImageKit from "imagekit"
 
 // Initialize ImageKit
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 })
 
 // Add a new product

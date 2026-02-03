884e9a4624b6a229f63aa79197ec1e7f74cdb85c
Harshita <harshitarera@gmail.com>
2026-02-03 12:16:58 +0530

    update

--- app/api/address/route.js
+++ app/api/address/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 import dbConnect from "@/lib/mongodb"
 import Address from "@/lib/models/Address"
 
-// Add new address to db
+// Add new address
 export async function POST(request) {
     try {
         await dbConnect()
